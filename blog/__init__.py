94614fdeff1ccd5edf8b4ec783c1f9669fceb4ce
ghostlinker <327284110@qq.com>
2019-05-14 21:25:22 +0800

    对settings.py以及默认db进行设置

---
+++ blog/__init__.py
@@ -0,0 +1,3 @@
+# 将mysql作为默认DB
+import pymysql
+pymysql.install_as_MySQLdb()
--- myblog/settings.py
+++ myblog/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'Svc_2003',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
@@ -92,18 +96,25 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+# media路径配置
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
